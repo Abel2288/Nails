c64ab4d3f2b4b62ae76d3979b0ec5cc302b1493e
Ábel Szabó <szabo.abel@petrik.hu>
2024-03-04 23:09:12 +0100

    Start2

--- app/src/main/java/com/example/nails/Register.java
+++ app/src/main/java/com/example/nails/Register.java
@@ -2,7 +2,9 @@ package com.example.nails;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,7 +18,14 @@ TextView tv;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this, MainActivity.class));
+            }
+        });
     }
+
     public void init()
     {
         edUsername=findViewById(R.id.editTextRegUsername);
