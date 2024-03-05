827c6713651c32d6be045a7d78a350934b4c8154
Ábel Szabó <szabo.abel@petrik.hu>
2024-03-05 15:50:53 +0100

    Start4

--- app/src/main/java/com/example/nails/MainActivity.java
+++ app/src/main/java/com/example/nails/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.nails;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,11 +31,25 @@ TextView tv1;
             public void onClick(View view) {
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
+                Database db = new Database(getApplicationContext(),"Costumers",null,1);
                 if (username.length() == 0 || password.length() == 0) {
                     Toast.makeText(getApplicationContext(), "Pleas fill all details", Toast.LENGTH_SHORT).show();
-
                 } else {
-                    Toast.makeText(getApplicationContext(), "Login Sucessfull", Toast.LENGTH_SHORT).show();
+                    if (db.login(username,password)==1)
+                    {
+                        Toast.makeText(getApplicationContext(), "Login Sucessfull", Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedPreferences =getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username",username);
+                        editor.apply();
+                        startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(getApplicationContext(), "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+
+                    }
+
                 }
             }
         });
